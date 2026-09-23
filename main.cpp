43e22b4487f45ccde4ae1c4647f9e4e09dcc5f1c
azumimicucu <zidniganzs@gmail.com>
2026-09-23 20:33:52 +0700

    revisi

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 
 void setup(){
   pinMode(PC13, OUTPUT);
+  //Inisiasi pin PC13 ke mode OUTPUT
 }
 void loop(){
   digitalWrite(PC13, HIGH);
+  //Menyalakan lampu melalui pin 13
   delay(5000);
   digitalWrite(PC13, LOW);
+  //Mematikan lampu melalui pin 13
   delay(1000);
 }
--- rgb.cpp
+++ rgb.cpp
@@ -2,12 +2,15 @@
 
 void setup(){
   Serial.begin(115200);
+  //Inisiasi Serial dengan kecepatan 115200
 }
 
 void loop(){
   Serial.println("Hello Dunia");
+  //Mencetak tulisan "Hallo Dunia" pada serial Monitor
   delay(1000);
   neopixelWrite(RGB_BUILTIN, 255, 0, 0);
+  //Menyalakan lampu builltin yang ada di esp32-s3
   delay(1000);
   neopixelWrite(RGB_BUILTIN, 0, 255, 0);
   delay(1000);
