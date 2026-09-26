492fc415d9e135213fdac350f6caf3895338a969
RazanMazayaA <razan.mazaya.contact@gmail.com>
2026-09-27 00:07:48 +0700

    Day-3 Blakasutha: ESP32

---
+++ esp32.ino
@@ -0,0 +1,14 @@
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() 
+{
+  neopixelWrite(LED_BUILTIN, 255,0,0);
+  delay(1000);
+  neopixelWrite(LED_BUILTIN, 0,255,0);
+  delay(1000);
+  neopixelWrite(LED_BUILTIN, 0,0,255);
+  delay(1000);
+}
